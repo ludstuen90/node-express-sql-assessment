b69d4dc0cb7e3bd4696d111df3ceab9144323a21
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-06-17 16:27:22 -0500

    add to database now works

--- public/scripts/clientApp.js
+++ public/scripts/clientApp.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   console.log('Doge');
 
+
+  $(document).on('click', '#showMe', function(){
+    console.log("Show me clicked!");
+    getAnimals();
+  });
+
+
   $('#zooInputField').on('click', '#animalSub', function(){
     var logMe = $('#animalIn').val();
     console.log("So, we have just captured " + logMe);
@@ -8,17 +15,48 @@ $(document).ready(function(){
     var exportAnimal={
       "newAnimalIn": logMe
     };
-$.ajax({
-  type: 'POST',
-  url:'/newAnimal',
-  data: exportAnimal,
-  success: function(){
-    console.log("Huzzah! Ajax success!");
-  },
-  error: function(){
-    console.log("Argh, the seas are stormy today. Ajax failed.");
-  }
+        $.ajax({
+          type: 'POST',
+          url:'/newAnimal',
+          data: exportAnimal,
+          success: function(){
+            console.log("Huzzah! Ajax success!");
+
+          },
+          error: function(){
+            console.log("Argh, the seas are stormy today. Ajax failed.");
+          }
+      });
+      getAnimals();
 });
 
+
+var getAnimals = function(){
+  $.ajax({
+    type: 'GET',
+    url: '/getAnimals',
+    success: function(data){
+      // updatePage(data);
+      updatePage(data);
+      console.log(data);
+    },
+    error: function(){
+      console.log("Man, the getAnimals function didn't work.");
+    }
   });
+var updatePage=function(data){
+  console.log("Made it to update page!");
+  $('#animalsInDatabase').empty();
+  $('#animalsInDatabase').append('<table>');
+  $('#animalsInDatabase').append('<tr><td><b>Animal</b></td>' +' <td><b>Count</b></td> </tr></b>');
+
+  for (var i = 0; i<data.length; i++){
+    $('#animalsInDatabase').append('<tr>');
+    $('#animalsInDatabase').append('<td><p>' + data[i].animal_name + '</td><td>    ' + data[i].animal_count +'</td></p>');
+  }
+  $('#animalsInDatabase').append('</table>');
+
+};
+};
+
 });
--- server/app.js
+++ server/app.js
@@ -3,7 +3,13 @@ var app=express();
 var path=require('path');
 var bodyParser=require('body-parser');
 var urlencodedParser=bodyParser.urlencoded({extended:false});
+
+//incorporates modules
+var randomNumber=require('../modules/randomNumber.js');
+
+//adds database connectivity
 var pg=require('pg');
+var connectionString= 'postgres://localhost:5432/zoo';
 
 app.use(express.static('public'));
 
@@ -15,7 +21,36 @@ app.get('/', function(req, res){
 
 app.post('/newAnimal', urlencodedParser, function(req, res){
   console.log('In newAnimal, we have just received: ' + req.body.newAnimalIn);
+  var undecided=randomNumber(1,100);
+  pg.connect(connectionString, function(err, client, done){
+    client.query('INSERT INTO zoo_table (animal_name, animal_count) VALUES($1, $2)', [req.body.newAnimalIn, undecided]);
+  });
 });
+
+app.get('/randomNumber', function(req, res){
+  var undecided=randomNumber(1,100);
+  res.write('our random number generator is working, and has returned: '+ undecided);
+  res.end();
+});
+
+app.get('/getAnimals', function(req, res){
+  console.log("received request for getAnimals");
+  results = [];
+  pg.connect(connectionString, function(err, client, done){
+    var query = client.query('SELECT animal_name, animal_count FROM zoo_table;');
+    query.on('row', function(row){
+      results.push(row);
+    });//end row
+    //close connection
+    query.on('end', function(){
+      done();
+      return res.json(results);
+    });
+    if(err){
+      console.log(err);
+    }//end error
+  }); //end connect
+}); //end get Animals
 
 app.listen(3000, 'localhost', function(req, res){
   console.log('server is now listening at port 3000');
